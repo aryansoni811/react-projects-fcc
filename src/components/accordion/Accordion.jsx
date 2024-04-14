95d3277c0f7770f72f0c683f5c86cce13931a16c
Aryan Soni <arpitsoni811@gmail.com>
2024-04-14 21:33:39 +0530

    accordion complete

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -5,22 +5,26 @@ const Accordion = () => {
   const [selected, setSelected] = useState(null);
   const handleSingleSelection = (currentID) => {
     console.log(currentID);
-    setSelected(currentID);
+    setSelected(currentID === selected ? null : currentID);
   };
-
+  console.log(selected);
   return (
     <>
       <h1>Accordion</h1>
       <h1>Selected = {selected}</h1>
-      <div className="wrapper">
-        <div className="accordion-container">
+      <div className="wrapper text-center ">
+        <div className="accordion-container flex flex-col gap-3">
           {data && data.length > 0 ? (
             data.map((dataItem) => (
-              <div className="item">
-                <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
+              <div className="item border border-black">
+                <div
+                  onClick={() => handleSingleSelection(dataItem.id)}
+                  className="title border border-black flex space justify-between font-bold"
+                >
                   <h3>{dataItem.question}</h3>
                   <span>+</span>
                 </div>
+                {selected === dataItem.id ? <div className="content">{dataItem.answer}</div> : null}
               </div>
             ))
           ) : (
